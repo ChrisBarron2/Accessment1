ce5c6cf89786bc164fd67825dae0d5e240b857e8
COR Students <corstudents@Matts-MBP-9.localdomain>
2018-10-22 12:26:33 -0400

    finished accessment1

---
+++ Access.js
@@ -0,0 +1,123 @@
+// let Chris = Name (string) 
+// const age = Chris // Int
+// let pi =3.14   float doubles
+  
+// let trueFlase = true; // bool
+
+
+// logic Statments 
+// Check to see if a statment is true
+// //Checks to see if a value is false
+// if(!trueOrfalse) {
+//   return "false"
+
+//   // default value if the first condition isnt met
+  
+// } else {
+//      return "true"
+// }
+
+// let favoriteColor === "red"
+// if(favoriteColor === "blue") {
+
+//     return "blue";
+    
+// }
+//   else if (favoriteColor === "green") {
+//       return  "green";
+
+//   } else if (favoriteColor === "red") {
+//           return "red"
+
+//   }
+// Loops
+// while(trueOrFalse){        It will run until you end it (until you put in a   counter)
+//     console.log("true");
+//      counter ++;
+//      If(counter ===10) {
+//          trueOrFalse =false;
+//      }
+// }
+
+
+// for( i -0; i <=100; i++) {
+//     console.log(i);
+
+// }
+
+// for( i =1000; i <=0; i++) {
+//     console.log(i);
+
+// }
+ 
+// let numbers = [1,2,3,4,5,,.7,8,9,10];
+// for( i -0; i <=numbers.lenght; i++) {
+//     console.log(numbers){i};
+   
+//    console.log(i)
+
+
+// }
+
+
+// let red = redOrVlue(false);
+
+// function redOrBlue(value) {
+//  if(value===true) {
+//   return "blue"
+
+// } else {
+//     return "red"
+
+// }
+// if(red=="red") {
+
+// //     for(i = 0; <numbers.lenght; i ++})
+// //     console.log(numbers{i}s);
+// }
+// //Array Methods
+// nunbers.sort ();
+// Number.split ("");
+// number.splcie(4,0,8);
+// number.push (11);
+// number.pop (11)
+// number.shift (puts in in there)
+// number.unshft (takes away)
+// number.reduce ();
+// number.map();
+// number.filter();
+// 
+
+//Acrunony for writing the same (D.R.Y Don't repeat yourself")
+
+//functions
+
+// function redOrBlue(value) {
+//  if(value===true) {
+//   return "blue"
+
+// } else {
+//     return "red"
+
+// }
+// if(red ==="red") 
+
+
+
+
+
+// Questions 1: Create a vairble of each of the following date type String, Int, Bool, Float.
+
+// Question 2 Create a varible called trueOrFalse.Create an if statment so that if the varible is true the number is returned.  Otherwise the number 2 is returned
+
+
+// Question 3: Create a loop that counts EVEN number from 1-100 starting at 0.
+
+// Question 4: Create a varible that is a bool and is equal to true Create a while loop that will print "hello" 
+// to the console 20 times and then will chnage that varible to false ending the loop.a
+
+// Question 5 Create a function that can determine if a paramter is true or false.  If true return a sting true Otherwise return a string of false.a
+
+// What does the question D.R.Y stand for? 
+
+
---
+++ Accessment1.js
@@ -0,0 +1,67 @@
+//Questions 1: Create a vairble of each of the following date type String, Int, Bool, Float.
+// (A) String are sequence of characters. In JavaScripting lanagage strings are treated as Objects.
+// Example of a String let name = Chris
+// (B) A Interger is a string number that cannot be changed. Example const age = 35
+// (C) Boolean tells you whether statment is true or False. Example let trueFlase = true;
+// (D) An example let pi =3.14 
+
+
+// Question 2 Create a varible called trueOrFalse.Create an if statment so that if the varible is true the number 1 is returned.  Otherwise the number 2 is returned
+
+// let true1 =  1;
+// let false2 = 2;
+
+
+// if("true1" === 1);{
+// console.log ("true");
+
+// }
+// {
+// else (true2 ===2);
+//       console.log("false");
+
+// }
+
+// Question 3: Create a loop that counts EVEN number from 1-100 starting at 0.
+
+// let even =1000
+
+// for(i = 1000; i <= number.lenght; 1++) {
+//     console.log(numbers[i]);
+// }
+
+
+//Question 4: Create a varible that is a bool and is equal to true Create a while loop that will print "hello" 
+// to the console 20 times and then will chnage that varible to false ending the loop.
+
+// let x = hello
+
+// while("hello"){        
+//          counter ++;
+//      If(counter ===20) {
+//       hello =false;
+//          }
+//     }
+
+
+// Question 5 Create a function that can determine if a paramter is true or false.  If true return a sting true Otherwise return a string of false.a
+
+
+// let x = ("true");
+// let y = ("false");
+
+// function trueOrFalse(parm) {
+//     if(x ===true)
+//     return "true";
+  
+// } else {
+//     return "false";
+
+// }
+
+
+
+
+//Bonus Question: stands for writing the same (D.R.Y Don't repeat yourself")
+
+
